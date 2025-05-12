e47978f61debb00f9d0d669492c595a7e468595a
Muqadas Iqbal <muqadasiqbal643@gmail.com>
2025-05-13 02:36:41 +0500

    Show header on login as well

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,63 +1,70 @@
-import React, { useContext } from 'react'
-import Login from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Admin/Dashboard';
-import AllAppointments from './pages/Admin/AllAppointments';
-import AddHerbalist from './pages/Admin/AddHerbalist';
-import HerbalistsList from './pages/Admin/HerbalistsList';
-import { AdminContext } from './context/AdminContext';
-import { HerbalistContext } from './context/HerbalistContext'; 
-import HerbalistDashboard from './pages/Herbalist/HerbalistDashboard';
-import HerbalistAppointments from './pages/Herbalist/HerbalistAppointments';
-import HerbalistProfile from './pages/Herbalist/HerbalistProfile';
-import AdminChats from './pages/Admin/AdminChats';
-import AdminChatRoom from './pages/Admin/AdminChatRoom';
-import HerbalistChats from './pages/Herbalist/HerbalistChats';
-import HerbalistChatRoom from './pages/Herbalist/HerbalistChatRoom';
-
-
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddHerbalist from "./pages/Admin/AddHerbalist";
+import HerbalistsList from "./pages/Admin/HerbalistsList";
+import { AdminContext } from "./context/AdminContext";
+import { HerbalistContext } from "./context/HerbalistContext";
+import HerbalistDashboard from "./pages/Herbalist/HerbalistDashboard";
+import HerbalistAppointments from "./pages/Herbalist/HerbalistAppointments";
+import HerbalistProfile from "./pages/Herbalist/HerbalistProfile";
+import AdminChats from "./pages/Admin/AdminChats";
+import AdminChatRoom from "./pages/Admin/AdminChatRoom";
+import HerbalistChats from "./pages/Herbalist/HerbalistChats";
+import HerbalistChatRoom from "./pages/Herbalist/HerbalistChatRoom";
 
 const App = () => {
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(HerbalistContext);
 
- const {aToken} = useContext(AdminContext)
- const {dToken} = useContext(HerbalistContext)
+  return (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      {aToken || dToken ? (
+        <div className="flex items-start">
+          <Sidebar />
+          <Routes>
+            {/* Admin Route */}
+            <Route path="/" element={<></>} />
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            <Route path="/all-appointment" element={<AllAppointments />} />
+            <Route path="/add-Herbalist" element={<AddHerbalist />} />
+            <Route path="/herbalist-list" element={<HerbalistsList />} />
+            <Route path="/admin-chats" element={<AdminChats />} />
+            <Route
+              path="/admin-chat/:appointmentId"
+              element={<AdminChatRoom />}
+            />
 
-  return aToken || dToken ? (
-    <div className='bg-[#F8F9FD]'> 
-      <ToastContainer/>
-      <Navbar/>
-      <div className='flex items-start'>
-        <Sidebar/>
-        <Routes>
-          {/* Admin Route */}
-          <Route path = '/' element={<></>} />
-          <Route path = '/admin-dashboard' element={<Dashboard/>} />
-          <Route path = '/all-appointment' element={<AllAppointments/>} />
-          <Route path = '/add-Herbalist' element={<AddHerbalist/>} />
-          <Route path = '/herbalist-list' element={<HerbalistsList/>} />
-          <Route path = '/admin-chats' element={<AdminChats/>} />
-          <Route path = '/admin-chat/:appointmentId' element={<AdminChatRoom/>} />
-
-          {/* Herbalist Route */}
-          <Route path = '/herbalist-dashboard' element={<HerbalistDashboard/>} />
-          <Route path = '/herbalist-appointments' element={<HerbalistAppointments/>} />
-          <Route path = '/herbalist-profile' element={<HerbalistProfile/>} />
-          <Route path = '/herbalist-chats' element={<HerbalistChats/>} />
-          <Route path = '/herbalist-chat/:appointmentId' element={<HerbalistChatRoom/>} />
-
-        </Routes>
-      </div>
+            {/* Herbalist Route */}
+            <Route
+              path="/herbalist-dashboard"
+              element={<HerbalistDashboard />}
+            />
+            <Route
+              path="/herbalist-appointments"
+              element={<HerbalistAppointments />}
+            />
+            <Route path="/herbalist-profile" element={<HerbalistProfile />} />
+            <Route path="/herbalist-chats" element={<HerbalistChats />} />
+            <Route
+              path="/herbalist-chat/:appointmentId"
+              element={<HerbalistChatRoom />}
+            />
+          </Routes>
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
-  ):(
-    <>
-    < Login />
-    <ToastContainer/>
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,33 +1,47 @@
-import React, { useContext } from 'react'
-import cure from '../assets/cure.png'
-import { AdminContext } from '../context/AdminContext'
-import { useNavigate } from 'react-router-dom'
-import { HerbalistContext } from '../context/HerbalistContext'
+import React, { useContext } from "react";
+import cure from "../assets/cure.png";
+import { AdminContext } from "../context/AdminContext";
+import { useNavigate } from "react-router-dom";
+import { HerbalistContext } from "../context/HerbalistContext";
 
 const Navbar = () => {
+  const { aToken, setAToken } = useContext(AdminContext);
+  const { dToken, setDToken } = useContext(HerbalistContext);
 
- const {aToken, setAToken} = useContext(AdminContext)
- const {dToken, setDToken} = useContext(HerbalistContext)
+  const navigate = useNavigate();
 
- const navigate = useNavigate()
+  const logout = () => {
+    navigate("/");
+    aToken && setAToken("");
+    aToken && localStorage.removeItem("aToken");
+    dToken && setDToken("");
+    dToken && localStorage.removeItem("dToken");
+  };
 
- const logout = () =>{
-       navigate('/')
-       aToken && setAToken('')
-       aToken && localStorage.removeItem('aToken')
-       dToken && setDToken('')
-       dToken && localStorage.removeItem('dToken')
- }
-
-  return(
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
-        <div className='flex items-center gap-2 text-xs'>
-            <img className='w-44 sm:w-50 cursor-pointer' src={cure} alt= "Admin Logo" /> 
-            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Herbalist'}</p> 
-        </div>
-        <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full' >Logout</button>
+  return (
+    <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
+      <div className="flex items-center gap-2 text-xs">
+        <img
+          className="w-44 sm:w-50 cursor-pointer"
+          src={cure}
+          alt="Admin Logo"
+        />
+        {(aToken || dToken) && (
+          <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
+            {aToken ? "Admin" : "Herbalist"}
+          </p>
+        )}
+      </div>
+      {(aToken || dToken) && (
+        <button
+          onClick={logout}
+          className="bg-primary text-white text-sm px-10 py-2 rounded-full"
+        >
+          Logout
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
